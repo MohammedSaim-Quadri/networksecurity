e78de2a84f17bb307b2c464f5fcef83f12328ee5
Mohammed Saim <mohammedsaimquadri@gmail.com>
2025-03-27 02:17:41 +0530

    model trainer & evaluation completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -16,6 +16,7 @@ TEST_FILE_NAME:str="test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
+SAVED_MODEL_DIR = os.path.join("saved_models")
 
 """
 Data ingestion related constant start with DATA_INGESTION VAR NAME
@@ -51,3 +52,13 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
 } 
 
 PREPROCESSING_OBJECT_FILE_NAME:str="preprocessing.pkl"
+
+
+"""
+Model Training related constant start with MODEL_TRAINER VAR NAME
+"""
+MODEL_TRAINER_DIR_NAME:str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR:str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME:str= "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE:float = 0.6
+MODEL_TRAINER_0VER_FITTING_UNDER_FITTING_THRESHOLD:float= 0.05
