43fcaa495d2664c61e49d38a7bdb76f975e1b10c
Mohammed Saim <mohammedsaimquadri@gmail.com>
2025-03-30 14:05:48 +0530

    training pipeline and fastapi code created

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -22,6 +22,9 @@ from sklearn.ensemble import (
 )
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='mohammedsaimquadri', repo_name="networksecurity",mlflow=True)
+
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig, data_transformation_artifact:DataTransformationArtifact):
@@ -123,6 +126,8 @@ class ModelTrainer:
         network_model = NetworkModel(preprocessor=preprocessor, model=best_model)
 
         save_object(self.model_trainer_config.trained_model_file_path, obj=network_model)
+
+        save_object("final_models/model.pkl", best_model)
 
         # model trainer artifact
         model_trainer_artifact = ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
